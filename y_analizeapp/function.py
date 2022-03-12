7551ab7c0953223f0621a04ca5895b34177b03e8
hideonSOS <code1555@icloud.com>
2022-03-12 22:12:34 +0900

    test

--- y_analizeapp/function.py
+++ y_analizeapp/function.py
@@ -2,12 +2,20 @@ import pandas as pd
 import numpy as np
 targetpath = 'model/20210804.csv'
 
-df = pd.read_csv(targetpath, index_col=1)
+df = pd.read_csv(targetpath, index_col=1,encoding='utf-8')
 
-def send_page1():
+def send_page1(inputbox1,inputbox2):
     page1_dict={
-        'Title':'SAMPLE TITLE',
+        'Title':inputbox1,
         'View':[i for i in df.iloc[:, 2]],
         'Axis':[i for i in df.iloc[:, 0]],
     }
     return page1_dict
+
+def send_page3():
+    page3_dict={
+        'Title':'SAMPLE TITLE',
+        'View':[i for i in df.iloc[:, 2]],
+        'Axis':[i for i in df.iloc[:, 0]],
+    }
+    return page3_dict
--- y_analizeapp/views.py
+++ y_analizeapp/views.py
@@ -6,10 +6,16 @@ def test(request):
     return render(request,'y_analizeapp/template.html')
 
 def page1(request):
-    page1_dict = function.send_page1()
-    print(page1_dict)
+    inputbox1 = request.POST.get('inputbox1')
+    inputbox2 = request.POST.get('inputbox2')
+    page1_dict = function.send_page1(inputbox1,inputbox2)
+    
     return render(request,'y_analizeapp/page1.html', {'page1_dict':page1_dict})
 
 def page2(request):
-    # function.test()
     return render(request,'y_analizeapp/page2.html')
+
+def page3(request): 
+    
+    page3_dict = function.send_page3()
+    return render(request,'y_analizeapp/page3.html',{'page3_dict':page3_dict})
