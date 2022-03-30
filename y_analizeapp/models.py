e3305023bac60fa043d9594a05dd4e10aea6dd55
hideonSOS <code1555@icloud.com>
2022-03-30 22:33:08 +0900

    test

--- y_analizeapp/models.py
+++ y_analizeapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from datetime import date
+from django.utils.timezone import now
 
-# Create your models here.
+class test_database(models.Model):
+    int_eria = models.IntegerField(default=0)
+    str_eria = models.CharField(max_length=10)
+    def __str__(self):
+        return self.name
