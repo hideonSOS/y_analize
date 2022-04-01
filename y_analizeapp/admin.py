fdbe8ad030ce26f65318ed856b8864636f613105
hideonSOS <code1555@icloud.com>
2022-04-02 00:43:22 +0900

    test

--- y_analizeapp/admin.py
+++ y_analizeapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import test_database
 
-# Register your models here.
+admin.register(test_database)
--- y_analizeapp/models.py
+++ y_analizeapp/models.py
@@ -5,5 +5,6 @@ from django.utils.timezone import now
 class test_database(models.Model):
     int_eria = models.IntegerField(default=0)
     str_eria = models.CharField(max_length=10)
+    str_eria = models.CharField(max_length=10)
     def __str__(self):
         return self.name
